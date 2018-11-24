16dd187a6a24b90088dbf3ee40aae373bdcf73c3
Nicolas Fuchs <nicolas.fuchs@edu.hefr.ch>
2018-11-24 15:46:46 +0100

    Code to retrieve images with the raspberry pi camera

---
+++ index.js
@@ -0,0 +1,33 @@
+const koa = require('koa');
+const app = new koa();
+
+const Raspistill = require('node-raspistill').Raspistill;
+const camera = new Raspistill({
+    width: 640,
+    height: 480,
+    //fileName: 'image'
+    noFileSave: true
+});
+
+let lastImage = '';
+
+cameraTimeLapse();
+setInterval(cameraTimeLapse,5000);
+
+function cameraTimeLapse() {
+    camera.timelapse(500, 5000, (image) => {
+	lastImage = image;
+	console.log(image);
+    }).then(() => {
+    }).catch((error) => {
+	console.log('An error occured with the raspberry pi camera');
+    });
+}
+
+app.use(async ctx => {
+    ctx.status = 200;
+    ctx.body = lastImage;
+});
+
+app.listen(8080);
+console.log('Yellow API Image Server started');
